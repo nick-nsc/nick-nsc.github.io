82221ee36eb7b3a4267c210b126a7e3646bc47d1
Nick <87150577+nick-nsc@users.noreply.github.com>
2022-05-05 23:01:59 +0200

    change bg-color

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Skills from "./components/Skills";
 
 const App = () => {
   return (
-    <main className="bg-white">
+    <main className="bg-gradient-to-b from-cyan-500 to-blue-500 min-h-screen">
       <NavBar />
       <About />
       <Projects />

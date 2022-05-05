06a4dbf8876fe7000a60294a03550e1d4599ba6e
Nick <87150577+nick-nsc@users.noreply.github.com>
2022-05-05 23:01:29 +0200

    refactor code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,27 @@
 const NavBar = () => {
-    return(
-        <nav className="px-2 sm:px-4 py-2 w-auto bg-gray-300">
+    return (
+        <nav className="px-2 sm:px-4 py-2 bg-gray-300">
             <ul className="flex flex-col sm:flex-row sm:space-x-8 justify-center">
-                <a href="#"><li className="py-2 px-3 text-white bg-blue-700 rounded">Home</li></a>
-                <a href="#about"><li className="py-2 px-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 sm:border-0 rounded">About</li></a>
-                <a href="#projects"><li className="py-2 px-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 sm:border-0 rounded">Projects</li></a>
-                <a href="#skills"><li className="py-2 px-3 text-gray-700 hover:bg-gray-50 rounded">Skills</li></a>
+                <a href="#">
+                    <li className="py-2 px-3 text-white bg-cyan-500 rounded">
+                        Home
+                    </li>
+                </a>
+                <a href="#about">
+                    <li className="py-2 px-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 sm:border-0 rounded">
+                        About
+                    </li>
+                </a>
+                <a href="#projects">
+                    <li className="py-2 px-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 sm:border-0 rounded">
+                        Projects
+                    </li>
+                </a>
+                <a href="#skills">
+                    <li className="py-2 px-3 text-gray-700 hover:bg-gray-50 rounded">
+                        Skills
+                    </li>
+                </a>
             </ul>
         </nav>
     )

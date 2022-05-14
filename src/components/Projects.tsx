62eb15a9f6e7235d2632bec646afac63c5c3616f
Nick <87150577+nick-nsc@users.noreply.github.com>
2022-05-14 23:48:20 +0200

    added project cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,102 @@
 const Projects = () => {
     return(
-        <div id="projects">
-            Projects
+        <div id="projects flex flex-col justify-center content-center">
+            <div className="flex px-4 py-5 sm:px-6 justify-center">
+                <h1 className="text-3xl font-medium text-gray-900">Projects</h1>
+            </div>
+            <div className="flex flex-col sm:flex-row sm:flex-wrap place-content-center">
+                <div className="sm:max-w-sm sm:w-1/3 mb-4 px-2">
+                    <div className="bg-white rounded border">
+                        <picture className="bg-gray-200 border-b">
+                            <img src="https://via.placeholder.com/800x600/EDF2F7/E2E8F0/&amp;text=Project" alt="Project" />
+                        </picture>
+                        <div className="p-4">
+                            <h3 className="text-lg font-bold">
+                                Project name
+                            </h3>
+                            <time className="block mb-2 text-sm text-gray-600">
+                                14th May 2022
+                            </time>
+                            <p>
+                                This is a project about...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="sm:max-w-sm sm:w-1/3 mb-4 px-2">
+                    <div className="bg-white rounded border">
+                        <picture className="bg-gray-200 border-b">
+                            <img src="https://via.placeholder.com/800x600/EDF2F7/E2E8F0/&amp;text=Project" alt="Project" />
+                        </picture>
+                        <div className="p-4">
+                            <h3 className="text-lg font-bold">
+                                Project name
+                            </h3>
+                            <time className="block mb-2 text-sm text-gray-600">
+                                14th May 2022
+                            </time>
+                            <p>
+                                This is a project about...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="sm:max-w-sm sm:w-1/3 mb-4 px-2">
+                    <div className="bg-white rounded border">
+                        <picture className="bg-gray-200 border-b">
+                            <img src="https://via.placeholder.com/800x600/EDF2F7/E2E8F0/&amp;text=Project" alt="Project" />
+                        </picture>
+                        <div className="p-4">
+                            <h3 className="text-lg font-bold">
+                                Project name
+                            </h3>
+                            <time className="block mb-2 text-sm text-gray-600">
+                                14th May 2022
+                            </time>
+                            <p>
+                                This is a project about...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="sm:max-w-sm sm:w-1/3 mb-4 px-2">
+                    <div className="bg-white rounded border">
+                        <picture className="bg-gray-200 border-b">
+                            <img src="https://via.placeholder.com/800x600/EDF2F7/E2E8F0/&amp;text=Project" alt="Project" />
+                        </picture>
+                        <div className="p-4">
+                            <h3 className="text-lg font-bold">
+                                Project name
+                            </h3>
+                            <time className="block mb-2 text-sm text-gray-600">
+                                14th May 2022
+                            </time>
+                            <p>
+                                This is a project about...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="sm:max-w-sm sm:w-1/3 mb-4 px-2">
+                    <div className="bg-white rounded border">
+                        <picture className="bg-gray-200 border-b">
+                            <img src="https://via.placeholder.com/800x600/EDF2F7/E2E8F0/&amp;text=Project" alt="Project" />
+                        </picture>
+                        <div className="p-4">
+                            <h3 className="text-lg font-bold">
+                                Project name
+                            </h3>
+                            <time className="block mb-2 text-sm text-gray-600">
+                                14th May 2022
+                            </time>
+                            <p>
+                                This is a project about...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
